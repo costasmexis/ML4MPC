3cc9c3f3f26a6216676f7176411b03a210180e0f
comex <costasmexis@gmail.com>
2025-01-29 15:06:43 +0200

    'Update'

--- src/machinelearning.py
+++ src/machinelearning.py
@@ -7,13 +7,17 @@ from tqdm import tqdm
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+# --- Parameters ---
+LEARNING_RATE = 0.01
+VERBOSE = 1000
+
 # Define the neural network
 class NeuralNet(nn.Module):
     def __init__(self, input_dim: int, output_dim: int):
         super(NeuralNet, self).__init__()
-        self.fc1 = nn.Linear(input_dim, 50)
-        self.hidden = nn.Linear(50, 50)
-        self.fc2 = nn.Linear(50, output_dim)
+        self.fc1 = nn.Linear(input_dim, 256)
+        self.hidden = nn.Linear(256, 256)
+        self.fc2 = nn.Linear(256, output_dim)
 
     def forward(self, x):
         x = torch.relu(self.fc1(x))
@@ -23,7 +27,7 @@ class NeuralNet(nn.Module):
 
 def train_nn(model: nn.Module, X: np.ndarray, y: np.ndarray, num_epochs: int = 1000) -> nn.Module:
     criterion = nn.MSELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     X_tensor = torch.tensor(X, dtype=torch.float32).to(device)
     y_tensor = torch.tensor(y, dtype=torch.float32).view(-1, 1).to(device)
@@ -36,14 +40,18 @@ def train_nn(model: nn.Module, X: np.ndarray, y: np.ndarray, num_epochs: int = 1
         loss.backward()
         optimizer.step()
 
-        if (epoch+1) % 100 == 0:
+        if (epoch+1) % VERBOSE == 0:
             print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
             
     return model
 
 def model_predict(Cb, w1, w2, model):
-    # Check if model is sklearn 
+    # Check if model is sklearn or pytorch
     if hasattr(model, 'predict'):
         return model.predict([[w1, Cb]])[0]
+    elif isinstance(model, nn.Module):
+        model.eval()
+        with torch.no_grad():
+            return model(torch.tensor([w1, Cb], dtype=torch.float32).to(device)).item()
     else:
         raise ValueError('Model is not supported')
