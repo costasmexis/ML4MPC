56afa8d27b025cf30dd84c37cfc26198218ad1d3
costasmexis <costasmexis@gmail.com>
2025-02-24 21:52:16 +0200

    Adding PINC to src/bioreactor.py

--- src/bioreactor.py
+++ src/bioreactor.py
@@ -1,5 +1,12 @@
+import copy
+import sys
+from typing import Union
+
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
 from scipy.integrate import solve_ivp
 from scipy.interpolate import interp1d
 from scipy.optimize import minimize
@@ -137,7 +144,7 @@ def plot_results(X, F):
     plt.grid()
 
     plt.subplot(2, 1, 2)
-    plt.plot(np.arange(0, TIME_RANGE, dt), F, label='Feed Rate')
+    plt.step(np.arange(0, TIME_RANGE, dt), F, label='Feed Rate')
     plt.legend()
     plt.grid()
 
@@ -186,7 +193,7 @@ def evaluation(F):
     plt.grid()
 
     plt.subplot(3, 1, 2)
-    plt.plot(sol_t, F_func(sol_t), label='Feed Rate')
+    plt.step(sol_t, F_func(sol_t), label='Feed Rate')
     plt.legend()
     plt.grid()
     
@@ -232,13 +239,160 @@ def generate_training_data():
             
     return np.array(X), np.array(y)
 
+   
+####### Physics-Informed Neural Network #######
+NUM_EPOCHS = 50000
+LEARNING_RATE = 1e-2
+NUM_COLLOCATION = 10000
+PATIENCE = 1000
+THRESHOLD = 1e-3
+EARLY_STOPPING_EPOCH = 1
+NUM_SAMPLES = 1000
+
+X_MIN = 0.01
+X_MAX = 15.0
+S_MIN = 0.10
+S_MAX = 20.0
+V_MIN = 0.5
+V_MAX = 5.0
+F_MIN = 0.01
+F_MAX = 2
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+def numpy_to_tensor(array):
+    return torch.tensor(array, requires_grad=True, dtype=torch.float32).to(DEVICE).reshape(-1, 1)
+
+def grad(outputs, inputs):
+    return torch.autograd.grad(outputs, inputs, grad_outputs=torch.ones_like(outputs), create_graph=True)
+
+class PINN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(PINN, self).__init__()
+        self.input = nn.Linear(input_dim, 64)
+        self.fc1 = nn.Linear(64, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.fc3 = nn.Linear(256, 64)
+        self.output = nn.Linear(64, output_dim)
+
+    def forward(self, x):
+        x = torch.tanh(self.input(x))
+        x = torch.tanh(self.fc1(x))
+        x = torch.tanh(self.fc2(x))
+        x = torch.tanh(self.fc2(x))
+        x = torch.tanh(self.fc3(x))
+        x = self.output(x)
+        return x
+    
+def generate_dataset(num_samples: int = NUM_SAMPLES):
+    """Generate dataset of random multiple initial conditions and control actions"""
+    df = pd.DataFrame(columns=['t', 'X', 'S', 'V'])
+    df['X'] = np.random.uniform(X_MIN, X_MAX, num_samples)
+    df['S'] = np.random.uniform(S_MIN, S_MAX, num_samples)
+    df['V'] = np.random.uniform(V_MIN, V_MAX, num_samples)
+    df['F'] = np.random.uniform(F_MIN, F_MAX, num_samples)
+    df['t'] = 0.0 # initial time
+    
+    t_train = numpy_to_tensor(df['t'].values)
+    X_train = numpy_to_tensor(df['X'].values)
+    S_train = numpy_to_tensor(df['S'].values)
+    V_train = numpy_to_tensor(df['V'].values)
+    F_train = numpy_to_tensor(df['F'].values)
+    
+    in_train = torch.cat([t_train, X_train, S_train, V_train, F_train], dim=1)
+    out_train = torch.cat([X_train, S_train, V_train], dim=1)
+    
+    return in_train, out_train
+
+def loss_fn(net: nn.Module) -> torch.Tensor:
+    t_col = numpy_to_tensor(np.random.uniform(T_START, dt, NUM_COLLOCATION))
+    X0_col = numpy_to_tensor(np.random.uniform(X_MIN, X_MAX, NUM_COLLOCATION))
+    S0_col = numpy_to_tensor(np.random.uniform(S_MIN, S_MAX, NUM_COLLOCATION))
+    V0_col = numpy_to_tensor(np.random.uniform(V_MIN, V_MAX, NUM_COLLOCATION))
+    F_col = numpy_to_tensor(np.random.uniform(F_MIN, F_MAX, NUM_COLLOCATION))
+    
+    u_col = torch.cat([t_col, X0_col, S0_col, V0_col, F_col], dim=1)
+
+    preds = net.forward(u_col)
+
+    X_pred = preds[:, 0].view(-1, 1)
+    S_pred = preds[:, 1].view(-1, 1)
+    V_pred = preds[:, 2].view(-1, 1)
+
+    dXdt_pred = grad(X_pred, t_col)[0]
+    dSdt_pred = grad(S_pred, t_col)[0]
+    dVdt_pred = grad(V_pred, t_col)[0]
+
+    mu = MU_MAX * S_pred / (K_S + S_pred)
+
+    error_dXdt = dXdt_pred - mu * X_pred + X_pred * F_col / V0_col
+    error_dSdt = dSdt_pred + mu * X_pred / Y_XS - F_col / V0_col * (S_F - S_pred)
+    error_dVdt = dVdt_pred - F_col
+    
+    error_ode = 1/3 * torch.mean(error_dXdt**2) + 1/3 * torch.mean(error_dSdt**2) + 1/3 * torch.mean(error_dVdt**2)
+
+    return error_ode
+
+def main(in_train: torch.Tensor, out_train: torch.Tensor, verbose: int = 100):
+    
+    net = PINN(input_dim=5, output_dim=3).to(DEVICE)
+    optimizer = torch.optim.Adam(net.parameters(), lr=LEARNING_RATE)
+    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=2500, gamma=0.7)
+
+    # Loss weights
+    w_data, w_ode, w_ic = 1.0, 1.0, 1.0
+
+    # Initialize early stopping variables
+    best_loss = float("inf")
+    best_model_weights = None
+    patience = PATIENCE
+    threshold = THRESHOLD
+
+    for epoch in range(NUM_EPOCHS):
+        optimizer.zero_grad()
+        preds = net.forward(in_train)
+        X_pred = preds[:, 0].view(-1, 1)
+        S_pred = preds[:, 1].view(-1, 1)
+        V_pred = preds[:, 2].view(-1, 1)
+        loss_X = nn.MSELoss()(X_pred, out_train[:, 0].view(-1, 1))
+        loss_S = nn.MSELoss()(S_pred, out_train[:, 1].view(-1, 1))
+        loss_V = nn.MSELoss()(V_pred, out_train[:, 2].view(-1, 1))
+        loss_data = 0.33 * (loss_X + loss_S + loss_V)
+
+        loss_ode = loss_fn(net)
+
+        loss = w_data * loss_data + w_ode * loss_ode
+        loss.backward()
+        optimizer.step()
+        scheduler.step()
+
+        if epoch % verbose == 0:
+            print(f"Epoch {epoch}, Loss_data: {loss_data.item():.4f}, Loss_ode: {loss_ode.item():.4f}")
+            # Print the current learning rate of the optimizer
+            for param_group in optimizer.param_groups:
+                print("Current learning rate: ", param_group["lr"])
+
+        if epoch >= EARLY_STOPPING_EPOCH:
+            if loss < best_loss - threshold:
+                best_loss = loss
+                best_model_weights = copy.deepcopy(net.state_dict())
+                patience = 1000
+            else:
+                patience -= 1
+                if patience == 0:
+                    print(f"Early stopping at epoch {epoch}")
+                    net.load_state_dict(best_model_weights)
+                    break
+
+    return net
+
 # --- Model predition --- #
 def model_predict(X, S, V, F, model):
     if hasattr(model, 'predict'):
         return model.predict([[X, S, V, F]])[0]
-    # elif isinstance(model, nn.Module):
-    #     model.eval()
-    #     with torch.no_grad():
-    #         return model(torch.tensor([X, S, V, F], dtype=torch.float32).to(device)).item()
+    elif isinstance(model, nn.Module):
+        model.eval()
+        with torch.no_grad():
+            return model(torch.tensor([X, S, V, F], dtype=torch.float32).to(device)).item()
     else:
         raise ValueError('Model is not supported')
